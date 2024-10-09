5ddd8fd7c14618d575abec2aa4d6f0541f97d0e5
wayangalihpratama <wgprtm@gmail.com>
2024-10-09 15:02:48 +0800

    Init open/create a spreadsheet

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+import os
 import gspread
+
 from google.oauth2.service_account import Credentials
+
+SPREADSHEET_NAME = os.getenv("SPREADSHEET_NAME", "example-spreadsheet")
+SHARE_TO_EMAIL = os.getenv("SHARE_TO_EMAIL", None)
 
 scopes = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,4 +17,14 @@ credentials = Credentials.from_service_account_file(
 
 gc = gspread.authorize(credentials)
 
-print(gc)
+try:
+    sh = gc.open(SPREADSHEET_NAME)
+except Exception:
+    print(f"Creating new sheet: {SPREADSHEET_NAME}")
+    sh = gc.create(SPREADSHEET_NAME)
+    if SHARE_TO_EMAIL:
+        sh.share(SHARE_TO_EMAIL, perm_type="user", role="writer")
+
+worksheet_list = sh.worksheets()
+
+print(worksheet_list)
